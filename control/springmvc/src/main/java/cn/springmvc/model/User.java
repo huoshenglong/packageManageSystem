ae3bd52134e74e3e225a2ca7622f7da2f1a55d72
long <1402992743@qq.com>
2018-03-14 17:12:20 +0800

    register

--- control/springmvc/src/main/java/cn/springmvc/model/User.java
+++ control/springmvc/src/main/java/cn/springmvc/model/User.java
@@ -9,6 +9,20 @@ public class User {
 	private int id;
 	private String username;
 	private String password;
+	private String e_position;
+	private double e_number;
+	public String getE_position() {
+		return e_position;
+	}
+	public void setE_position(String e_position) {
+		this.e_position = e_position;
+	}
+	public double getE_number() {
+		return e_number;
+	}
+	public void setE_number(double e_number) {
+		this.e_number = e_number;
+	}
 	public int getId() {
 		return id;
 	}
